bac547d2612c9e108af721e914251e9817c51926
Tanxiao <13671985248@163.com>
2022-11-30 09:26:43 +0800

    主页类型动态加载完成

--- src/main/java/org/bsm/bsm/controller/ViewController.java
+++ src/main/java/org/bsm/bsm/controller/ViewController.java
@@ -9,6 +9,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class ViewController {
     @Autowired
@@ -19,7 +21,11 @@ public class ViewController {
     @RequestMapping(value = "/",method = RequestMethod.GET)
     public String home(Model model){
         model.addAttribute("messageHome",new GsonBuilder().setDateFormat("yyyy年MM月dd日 HH:mm:ss").create().toJson(messageServiceIml.getMessageHome()));
-        model.addAttribute("typeHome",new  GsonBuilder().create().toJson(typeServiceIml.getAllType()));
         return "home.html";
+    }
+    @RequestMapping(value = "search",method = RequestMethod.GET)
+    public String search(Model model, HttpServletRequest request){
+        model.addAttribute("type",request.getParameter("type"));
+        return "search.html";
     }
 }
