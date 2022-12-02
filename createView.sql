56cac531146ead0c6bbdc4fbfb0977bbf5c4073c
Tanxiao <13671985248@163.com>
2022-12-02 15:59:38 +0800

    sql使用

--- createView.sql
+++ createView.sql
@@ -1,15 +1,3 @@
-CREATE VIEW `book_type` AS select Bid,Bname,Bauthor,Bprice,Bcount,Bpress,Bresume,Tname from type,bsm.book  where (book.Tid = type.Tid);
 
-
-DROP TRIGGER IF EXISTS `bsm`.`address_BEFORE_INSERT`;
-
-DELIMITER $$
-USE `bsm`$$
-CREATE DEFINER=`root`@`localhost` TRIGGER `address_BEFORE_INSERT` BEFORE INSERT ON `address` FOR EACH ROW BEGIN
-	set @maxget = (select max(Aid) from bsm.Address
-    );
-	set new.Aid = ifnull(@maxget + 1,1);
-END$$
-DELIMITER ;
 
 
--- createtable.sql
+++ createtable.sql
@@ -143,3 +143,17 @@ SELECT Uname , Uid ,Ubirth,Usex,Uaddress,Uphone,Uphone FROM bsm.user;
 CREATE  OR REPLACE VIEW `user_log_in` AS (select Uid, Uname, UPword from bsm.user );
 
 CREATE VIEW `message_show` AS (select Mid, Uname, Mword, Mtime, Mread from bsm.message, bsm.user where message.Uid = user.Uid)
+
+CREATE VIEW `book_type` AS select Bid,Bname,Bauthor,Bprice,Bcount,Bpress,Bresume,Tname from type,bsm.book  where (book.Tid = type.Tid);
+
+
+DROP TRIGGER IF EXISTS `bsm`.`address_BEFORE_INSERT`;
+
+DELIMITER $$
+USE `bsm`$$
+CREATE DEFINER=`root`@`localhost` TRIGGER `address_BEFORE_INSERT` BEFORE INSERT ON `address` FOR EACH ROW BEGIN
+	set @maxget = (select max(Aid) from bsm.Address
+    );
+	set new.Aid = ifnull(@maxget + 1,1);
+END$$
+DELIMITER ;
