280f6c2961562bee8eb6cd93ba9dfd82894fd535
Tanxiao <13671985248@163.com>
2022-12-06 20:59:19 +0800

    用户页面的检查与跳转完成

--- src/main/java/org/bsm/bsm/controller/ViewController.java
+++ src/main/java/org/bsm/bsm/controller/ViewController.java
@@ -2,9 +2,12 @@ package org.bsm.bsm.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.bsm.bsm.service.BookServiceIml;
 import org.bsm.bsm.util.SessionAttributeUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import javax.servlet.http.HttpServletRequest;
@@ -15,6 +18,8 @@ import javax.servlet.http.HttpSession;
 @Api(value = "页面跳转接口",description = "根据url检查")
 public class ViewController {
 
+    @Autowired
+    private BookServiceIml bookServiceIml;
 
     @RequestMapping(value = {"/","/home"},method = RequestMethod.GET)
     @ApiOperation(value = "主页跳转")
@@ -56,7 +61,30 @@ public class ViewController {
             return "person.html";
     }
 
-    @GetMapping(value = "book/*")
-    @ApiOperation(value = "书本跳转",notes = "用?bid=xxx表示具体是哪本书")
-    public String book() {return "book.html";}
+    @GetMapping(value = "book/{bid}")
+    @ApiOperation(value = "书本跳转",notes = "用/xxx表示具体是哪本书的bid,已主动判断是否存在")
+    public String book(@PathVariable String bid) {
+        if (bookServiceIml.queryOneBookWithType(bid)==null)
+            return "error.html";
+        else
+            return "book.html";
+    }
+
+    @GetMapping(value = "cart")
+    @ApiOperation(value = "购物车跳转",notes="检查是否登录，未登录进行跳转登录")
+    public String cart(HttpServletRequest request){
+        if (request.getSession().getAttribute(SessionAttributeUtil.getUserInfo())==null) {
+            return "redirect:/login";
+        }else
+            return "cart.html";
+    }
+
+    @GetMapping(value = "history")
+    @ApiOperation(value = "购物车跳转",notes="检查是否登录，未登录进行跳转登录")
+    public String history(HttpServletRequest request){
+        if (request.getSession().getAttribute(SessionAttributeUtil.getUserInfo())==null) {
+            return "redirect:/login";
+        }else
+            return "history.html";
+    }
 }
